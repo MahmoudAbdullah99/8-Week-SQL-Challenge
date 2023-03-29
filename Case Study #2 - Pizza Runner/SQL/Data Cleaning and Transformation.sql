9cda85effb1cfd093f12c71aed60990b8c3afedf
MahmoudAbdullah99 <mahmoudabdullah2079@gmail.com>
2023-03-29 17:59:54 +0200

    Made a clean temp table of runner_order table

--- Case Study #2 - Pizza Runner/SQL/Data Cleaning and Transformation.sql
+++ Case Study #2 - Pizza Runner/SQL/Data Cleaning and Transformation.sql
@@ -1,4 +1,4 @@
--- Create a temporary table with all the columns
+-- Create a temporary table of customer_orders with all the columns
 -- Remove null values in exlusions and extras columns and replace with blank space ''.
 
 CREATE TEMPORARY TABLE customer_orders_temp AS (
@@ -19,4 +19,39 @@ CREATE TEMPORARY TABLE customer_orders_temp AS (
             END) AS extras
     FROM
         pizza_runner.customer_orders
+);
+
+-- Create a temporary table of runner_orders with all the columns
+-- Remove null values in pickup_time, distance, duration, and cancellation columns and replace with blank space ''.
+-- Remove all unnecessary data like 'mins' and 'minutes' from duration columns and 'km' from distnce column.
+CREATE TEMPORARY TABLE runner_orders_temp AS (
+    SELECT
+        order_id,
+        runner_id,
+        (CASE
+            WHEN pickup_time IS NULL OR pickup_time LIKE 'null'
+                THEN ''
+            ELSE pickup_time
+            END) AS pickup_time,
+        (CASE
+            WHEN distance IS NULL OR distance LIKE 'null'
+                THEN ''
+            WHEN distance LIKE '%km'
+                THEN TRIM('km' FROM distance)
+            ELSE distance
+            END) AS distance,
+        (CASE
+            WHEN duration IS NULL OR duration LIKE 'null'
+                THEN ''
+            WHEN duration LIKE '%min%'
+                THEN LEFT(duration, STRPOS(duration, 'min')-1)
+            ELSE duration
+            END) AS duration,
+        (CASE
+            WHEN cancellation IS NULL OR cancellation LIKE 'null'
+                THEN ''
+            ELSE cancellation
+            END) AS cancellation
+    FROM
+        pizza_runner.runner_orders
 );
