2a0824e87e7a310dc191453392e0300bcb576c5a
MahmoudAbdullah99 <mahmoudabdullah2079@gmail.com>
2023-03-30 02:23:23 +0200

    Created a stored procedure to clean and transform customer_orders and runner_orders tables

--- Case Study #2 - Pizza Runner/SQL/Data Cleaning and Transformation.sql
+++ Case Study #2 - Pizza Runner/SQL/Data Cleaning and Transformation.sql
@@ -1,57 +1,65 @@
--- Create a temporary table of customer_orders with all the columns
--- Remove null values in exlusions and extras columns and replace with blank space ''.
+CREATE OR REPLACE PROCEDURE clean_transform_data()
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    -- Create temporary table of customer_orders
+    CREATE TEMPORARY TABLE customer_orders_temp AS (
+        SELECT
+            order_id,
+            customer_id,
+            pizza_id,
+            order_time,
+            (CASE
+                WHEN exclusions IS NULL OR exclusions LIKE 'null'
+                    THEN ''
+                ELSE exclusions
+                END) AS exclusions,
+            (CASE
+                WHEN extras IS NULL OR extras LIKE 'null'
+                    THEN ''
+                ELSE extras
+                END) AS extras
+        FROM
+            pizza_runner.customer_orders
+    );
 
-CREATE TEMPORARY TABLE customer_orders_temp AS (
-    SELECT
-        order_id,
-        customer_id,
-        pizza_id,
-        order_time,
-        (CASE
-            WHEN exclusions IS NULL OR exclusions LIKE 'null'
-                THEN ''
-            ELSE exclusions
-            END) AS exclusions,
-        (CASE
-            WHEN extras IS NULL OR extras LIKE 'null'
-                THEN ''
-            ELSE extras
-            END) AS extras
-    FROM
-        pizza_runner.customer_orders
-);
+    -- Create temporary table of runner_orders
+    CREATE TEMPORARY TABLE runner_orders_temp AS (
+        SELECT
+            order_id,
+            runner_id,
+            (CASE
+                WHEN pickup_time LIKE 'null'
+                    THEN NULL
+                ELSE pickup_time
+                END) AS pickup_time,
+            (CASE
+                WHEN distance LIKE 'null'
+                    THEN NULL
+                WHEN distance LIKE '%km'
+                    THEN TRIM('km' FROM distance)
+                ELSE distance
+                END) AS distance,
+            (CASE
+                WHEN duration LIKE 'null'
+                    THEN NULL
+                WHEN duration LIKE '%min%'
+                    THEN LEFT(duration, STRPOS(duration, 'min')-1)
+                ELSE duration
+                END) AS duration,
+            (CASE
+                WHEN cancellation LIKE 'null'
+                    THEN NULL
+                ELSE cancellation
+                END) AS cancellation
+        FROM
+            pizza_runner.runner_orders
+    );
 
--- Create a temporary table of runner_orders with all the columns
--- Remove null values in pickup_time, distance, duration, and cancellation columns and replace with blank space ''.
--- Remove all unnecessary data like 'mins' and 'minutes' from duration columns and 'km' from distnce column.
-CREATE TEMPORARY TABLE runner_orders_temp AS (
-    SELECT
-        order_id,
-        runner_id,
-        (CASE
-            WHEN pickup_time IS NULL OR pickup_time LIKE 'null'
-                THEN ''
-            ELSE pickup_time
-            END) AS pickup_time,
-        (CASE
-            WHEN distance IS NULL OR distance LIKE 'null'
-                THEN ''
-            WHEN distance LIKE '%km'
-                THEN TRIM('km' FROM distance)
-            ELSE distance
-            END) AS distance,
-        (CASE
-            WHEN duration IS NULL OR duration LIKE 'null'
-                THEN ''
-            WHEN duration LIKE '%min%'
-                THEN LEFT(duration, STRPOS(duration, 'min')-1)
-            ELSE duration
-            END) AS duration,
-        (CASE
-            WHEN cancellation IS NULL OR cancellation LIKE 'null'
-                THEN ''
-            ELSE cancellation
-            END) AS cancellation
-    FROM
-        pizza_runner.runner_orders
-);
+    -- Alter temporary table of runner_orders
+    ALTER TABLE runner_orders_temp
+    ALTER COLUMN pickup_time TYPE TIMESTAMP USING to_timestamp(pickup_time, 'YYYY-MM-DD HH24:MI:SS'),
+    ALTER COLUMN distance TYPE FLOAT USING distance::FLOAT,
+    ALTER COLUMN duration TYPE INT USING duration::INT;
+END;
+$$;
