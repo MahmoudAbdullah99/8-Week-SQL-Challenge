6c753f355c83c7fb88a803c808c0f641b3d07c55
MahmoudAbdullah99 <mahmoudabdullah2079@gmail.com>
2023-04-13 00:13:52 +0200

    Solved 5th question from section C case study 2

--- Case Study #2 - Pizza Runner/SQL/C.Ingredient Optimisation.sql
+++ Case Study #2 - Pizza Runner/SQL/C.Ingredient Optimisation.sql
@@ -63,13 +63,59 @@ SELECT cot.order_id,
             ELSE pn.pizza_name
            END) AS full_order
 FROM customer_orders_temp AS cot
-     INNER JOIN pizza_runner.pizza_names AS pn
-                ON cot.pizza_id = pn.pizza_id
-     LEFT JOIN ingredients_flat_cte AS cte
-               ON cot.order_id = cte.order_id
-     LEFT JOIN pizza_runner.pizza_toppings As top1
-               ON cte.extras = top1.topping_id::VARCHAR
-     LEFT JOIN pizza_runner.pizza_toppings As top2
-               ON cte.exclusions = top2.topping_id::VARCHAR
+         INNER JOIN pizza_runner.pizza_names AS pn
+                    ON cot.pizza_id = pn.pizza_id
+         LEFT JOIN ingredients_flat_cte AS cte
+                   ON cot.order_id = cte.order_id
+         LEFT JOIN pizza_runner.pizza_toppings As top1
+                   ON cte.extras = top1.topping_id::VARCHAR
+         LEFT JOIN pizza_runner.pizza_toppings As top2
+                   ON cte.exclusions = top2.topping_id::VARCHAR
 GROUP BY cot.order_id, pn.pizza_name, cot.exclusions, cot.extras
+;
+
+-- 5.Generate an alphabetically ordered comma separated ingredient list for each pizza order from the customer_orders table and add a 2x in front of any relevant ingredients
+--     For example: "Meat Lovers: 2xBacon, Beef, ... , Salami"
+WITH main_extra_cte AS (SELECT cot.order_id,
+                               cot.pizza_id,
+                               ROW_NUMBER() OVER (PARTITION BY cot.order_id) AS id,
+                               UNNEST(
+                                       ARRAY_REMOVE(
+                                               STRING_TO_ARRAY(
+                                                       CONCAT(pr.toppings, ', ', cot.extras)
+                                                   , ', ')
+                                           , '')
+                                   )::INT                                    AS ingredients
+                        FROM customer_orders_temp AS cot
+                                 INNER JOIN pizza_runner.pizza_recipes AS pr
+                                            ON cot.pizza_id = pr.pizza_id)
+
+SELECT cot.order_id, CONCAT(pn.pizza_name, ': ', ARRAY_TO_STRING(ARRAY_AGG(DISTINCT count_ingredient), ', '))
+FROM customer_orders_temp AS cot
+         INNER JOIN (SELECT cte.order_id,
+                            cte.pizza_id,
+                            cte.id,
+                            (CASE
+                                 WHEN count(*) <= 1
+                                     THEN top.topping_name
+                                 ELSE CONCAT('2x', top.topping_name) END) AS count_ingredient
+                     FROM main_extra_cte AS cte
+                              INNER JOIN pizza_runner.pizza_toppings AS top
+                                         ON cte.ingredients = top.topping_id
+                     WHERE cte.ingredients NOT IN (SELECT UNNEST(STRING_TO_ARRAY(exclusions, ','))::INT
+                                                   FROM customer_orders_temp AS cot
+                                                   WHERE cte.order_id = cot.order_id
+                                                     AND cte.pizza_id = cot.pizza_id)
+                     GROUP BY cte.order_id,
+                              cte.pizza_id,
+                              cte.id,
+                              top.topping_name
+                     ORDER BY cte.order_id,
+                              cte.pizza_id,
+                              cte.id,
+                              count_ingredient DESC) AS temp_counter
+                    ON cot.order_id = temp_counter.order_id
+                        AND cot.pizza_id = temp_counter.pizza_id
+         INNER JOIN pizza_runner.pizza_names AS pn ON cot.pizza_id = pn.pizza_id
+GROUP BY cot.order_id, pn.pizza_name, temp_counter.id
 ;
